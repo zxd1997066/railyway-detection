c7d59ebf957af2fca9756793080e5d1e6415f8cd
zxd1997066 <40376367+zxd1997066@users.noreply.github.com>
2019-04-24 17:14:49 +0800

    Add files via upload

--- getSubBunches.py
+++ getSubBunches.py
@@ -9,14 +9,12 @@ def getSubBunches(rgb,contours):
     for i in range(mask_len):
        subBunch[i].position = cv2.boundingRect(contours[i])
        subBunch[i].orientation = cv2.fitEllipse(contours[i])[2]
-       subBunch[i].rgb = rgb[int(subBunch[i].position[1]-1):int(subBunch[i].position[1]+subBunch[i].position[3]),int(subBunch[i].position[0]-1):int(subBunch[i].position[0]+subBunch[i].position[2])]
-       (B,G,R) = cv2.split(subBunch[i].rgb)
+       (B,G,R) = cv2.split(rgb)
        subBunch[i].mask = np.zeros(B.shape)
        cv2.drawContours(subBunch[i].mask,[contours[i]],-1,(255,255,255),-1)
-       for a in range(0,subBunch[i].mask.shape[0]):
-          for b in range(0,subBunch[i].mask.shape[1]):
-            if subBunch[i].mask[a][b] == 255:
-               subBunch[i].mask[a][b] =1
+       subBunch[i].mask = subBunch[i].mask[int(subBunch[i].position[1]-1):int(subBunch[i].position[1]+subBunch[i].position[3]),int(subBunch[i].position[0]-1):int(subBunch[i].position[0]+subBunch[i].position[2])]
+       subBunch[i].rgb = rgb[int(subBunch[i].position[1]-1):int(subBunch[i].position[1]+subBunch[i].position[3]),int(subBunch[i].position[0]-1):int(subBunch[i].position[0]+subBunch[i].position[2])]
+       cv2.imwrite('test4.jpg',subBunch[i].mask)
     return subBunch
 
 
